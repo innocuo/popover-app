7e00fd9adc73186451d4ef6ddc2f42bc94b34a0e
luis <info@innocuo.com>
2017-03-30 16:54:08 -0500

    Initial Commit

---
+++ popover-app/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  popover-app
+//
+//  Created by Luis Orozco on 3/30/17.
+//  Copyright © 2017 Luis Orozco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
